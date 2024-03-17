ed6f03d506846de0f5e86fa162eceb1ba306b715
endodog <endodog.dev@gmail.com>
2024-03-18 02:11:16 +0700

    update

--- src/main/java/com/endodog/timekeeping_management/TimekeepingManagementApplication.java
+++ src/main/java/com/endodog/timekeeping_management/TimekeepingManagementApplication.java
@@ -2,12 +2,13 @@ package com.endodog.timekeeping_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TimekeepingManagementApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(TimekeepingManagementApplication.class, args);
+    ApplicationContext applicationContext = SpringApplication.run(TimekeepingManagementApplication.class, args);
   }
 
 }
